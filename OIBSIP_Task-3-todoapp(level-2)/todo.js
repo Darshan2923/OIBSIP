41c82e6aebc57efe9bd1681ab4f3dddca8dddbc7
Darshan2923 <135990671+Darshan2923@users.noreply.github.com>
2023-06-08 20:06:59 +0530

    Add files via upload

---
+++ OIBSIP_Task-3-todoapp(level-2)/todo.js
@@ -0,0 +1,33 @@
+
+const input = document.getElementById('input');
+const list = document.getElementById('list-tasks');
+function addTask() {
+    if (input.value === '') {
+        alert("Please write something!!!");
+    }
+    else {
+        let li = document.createElement("li");
+        li.innerHTML = input.value;
+        list.appendChild(li);
+        let span = document.createElement("span");
+        span.innerHTML = "*";
+        li.appendChild(span);
+    }
+    input.value = "";
+    saveData();
+}
+list.addEventListener("click", function (e) {
+    if (e.target.tagName === "SPAN") {
+        e.target.parentElement.remove();
+        saveData();
+    }
+}, false)
+
+function saveData() {
+    localStorage.setItem("data", list.innerHTML);
+
+}
+function showTask() {
+    list.innerHTML = localStorage.getItem("data");
+}
+showTask();
